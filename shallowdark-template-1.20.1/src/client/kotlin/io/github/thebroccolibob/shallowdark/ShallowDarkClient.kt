a93edf476d78a4d25452658a2906d22e759d32f4
thebroccolibob <thebroccolibob@gmail.com>
2024-02-13 11:18:34 -0800

    Added Mod Template

---
+++ shallowdark-template-1.20.1/src/client/kotlin/io/github/thebroccolibob/shallowdark/ShallowDarkClient.kt
@@ -0,0 +1,9 @@
+package io.github.thebroccolibob.shallowdark
+
+import net.fabricmc.api.ClientModInitializer
+
+object ShallowDarkClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ shallowdark-template-1.20.1/src/main/kotlin/io/github/thebroccolibob/shallowdark/ShallowDarkDataGenerator.kt
@@ -0,0 +1,9 @@
+package io.github.thebroccolibob.shallowdark
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ShallowDarkDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
